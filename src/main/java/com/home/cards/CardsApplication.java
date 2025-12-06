d8924152b17a1c3734fa068e0330635c16af9d93
mhaidar <mhaidar@areeba.com>
2025-12-06 12:24:35 +0200

    configure environment-specific profiles and include build version logging

--- src/main/java/com/home/cards/CardsApplication.java
+++ src/main/java/com/home/cards/CardsApplication.java
@@ -1,6 +1,8 @@
 package com.home.cards;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,10 +10,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "Cards microservice", description = "Cards microservice REST APIs", version = "1.0"))
-public class CardsApplication {
+public class CardsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsApplication.class, args);
-	}
+    @Value("${build.version}")
+    private String buildVersion;
 
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        started();
+    }
+
+    void started() {
+        System.out.println("Application started with version: " + buildVersion);
+    }
 }
